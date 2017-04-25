75c684903133cf478edb585b1555968593f62655
soloproyectos <gonzalo.chumillas@gmail.com>
2017-04-25 20:05:56 +0200

    update dist.py script

--- bin/dist.py
+++ bin/dist.py
@@ -2,7 +2,12 @@
 
 """Updates distribution files"""
 
+import os
 import subprocess
+
+if ( not os.path.isdir('typings') ):
+	print "Install type definition files..."
+	subprocess.call(['bin/typings.py'])
 
 print "Updates JavaScript and Type Definition files..."
 subprocess.call(['rm', 'dist', '-rf'])
