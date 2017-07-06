1edc67ff28a0062674bba1aba147d6bba892944b
soloproyectos <gonzalo.chumillas@gmail.com>
2017-07-06 19:08:30 +0200

    codefix

--- src/Transformer.ts
+++ src/Transformer.ts
@@ -2,7 +2,7 @@
 
 import {SvgElement, SvgGraphicElement, SvgPath} from 'easyvg';
 import {SquareMatrix} from 'matrix';
-import {Line, Point, Vector, Positionable} from 'matrix2';
+import {Line, Point, Vector, Positionable, Transformation, rad2deg} from 'matrix2';
 
 // A decorator class to 'transform' (resize, scale or rotate) an SVG element.
 export class ElementTransformer {
@@ -86,8 +86,21 @@ export class ElementTransformer {
     canvas.addEventListener('mouseup', function (event) {
       p1 = new Point(event.offsetX, event.offsetY);
 
+      // calculates the center or the target from the canvas viewpoint
+      let box = self.target.nativeElement.getBBox();
+      let canvas = new SvgGraphicElement(self.target.nativeElement.ownerSVGElement);
+      let ctm = self.target.nativeElement.getCTM();
+      let t = Transformation.createFromValues(
+        ctm.a, ctm.b, ctm.c, ctm.d, ctm.e, ctm.f
+      );
+      let p2 = new Point(box.x + box.width / 2, box.y + box.width / 2)
+        .transform(t);
+
+      let alpha = _getAdjacentAngle(p0, p1, p2);
+      console.log(p0.toString(), p1.toString(), p2.toString());
+      console.log(rad2deg(alpha));
+
       /*
-      let c = self._getCenter();
       let v0 = Vector.createFromPoints(c, p0);
       let v1 = Vector.createFromPoints(p1, c);
       let l0 = new Line(p0, v0);
@@ -155,14 +168,6 @@ export class ElementTransformer {
     middleLeftHandle.position = new Point(box.x, box.y + box.height / 2);
     this._container.append(middleLeftHandle);
   }
-
-  // Gets the center of [this.target] relative to the SVG reference system.
-  private _getCenter(): Point {
-    let box = this.target.nativeElement.getBBox();
-    let c = new Point(box.x + box.width / 2, box.y + box.height / 2);
-
-    return c.transform(this.target.transformation);
-  }
 }
 
 class Handle extends SvgGraphicElement {
@@ -201,24 +206,22 @@ class Handle extends SvgGraphicElement {
 // the segment [p1, p2] is the minute hand. This function calculates the
 // angle enclosed between those two hands in the direction of the hands
 // of a clock.
-function getAdjacentAngle(p0: Point, p1: Point, p2: Point): number {
-	let v0 = Vector.createFromPoints(p2, p0);
-	let v1 = Vector.createFromPoints(p1, p2);
-	let l0 = new Line(p0, v0);
-	let l1 = new Line(p2, v1);
-	let l2 = l1.getPerpendicular(p0);
-	let p3 = l1.getIntersection(l2);
-	let u0 = Vector.createFromPoints(p3, p2).unit();
-	let u1 = Vector.createFromPoints(p0, p3).unit();
+function _getAdjacentAngle(p0: Point, p1: Point, p2: Point): number {
+  // creates an orthonormal reference system
+  let u = Vector.createFromPoints(p1, p2);
+	let u0 = u.unit();
+  let u1 = new Vector(u0.y, -u0.x);
+
+  // expresses the vector [p2, p0] from the orthonormal reference system
 	let v = Vector.createFromPoints(p0, p2);
 	let m = new SquareMatrix(u0, u1);
 	let w = v.multiply(m.inverse());
 
-	return getAngle(w);
+	return _getAngle(w);
 }
 
 // Gets the angle of a 'positionable' object.
-export function getAngle(p: Positionable): number {
+function _getAngle(p: Positionable): number {
 	let ret = NaN;
 	let [x, y] = [p.x, p.y];
 
